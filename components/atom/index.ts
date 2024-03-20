1ae32eebc806c08c2297221a1790bc3f9adf0ebc
amh6281 <amh6281@naver.com>
2024-03-20 18:57:52 +0900

    Feat: Atomic 폴더 구조 적용

---
+++ components/atom/index.ts
@@ -0,0 +1,4 @@
+export { default as Footer } from "./Footer";
+export { default as Navbar } from "./Navbar";
+export { default as Banner } from "./Banner";
+export { default as CategoryItem } from "./CategoryItem";
